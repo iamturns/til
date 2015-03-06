5fcb0d08333fc192d0bd32794e0ff54c212d47d0
Matthew Turnbull <turnbullm@gmail.com>
2015-03-06 15:45:57 +0000

    Javascript scroll to script

---
+++ dev/javascript/scroll-to.js
@@ -0,0 +1,23 @@
+// http://lions-mark.com/jquery/scrollTo/
+// $('body').scrollTo('#target');
+// $('#content').scrollTo(500);
+// $('body').scrollTo('#post-5', { duration:'slow', offsetTop : '50' });
+// $('body').scrollTo('#target', {}, complete_callback);
+
+$.fn.scrollTo = function( target, options, callback ) {
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 50,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
