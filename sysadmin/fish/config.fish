b319bdc7bc03fc011a1204ccb00668d8712ca10b
Matthew Turnbull <turnbullm@gmail.com>
2015-10-29 14:27:40 +0000

    Updated fish config

--- sysadmin/fish/config.fish
+++ sysadmin/fish/config.fish
@@ -46,3 +46,19 @@ function fish_prompt
   echo -n ' > '
   set_color normal
 end
+
+###
+# Fuck
+###
+
+function fuck -d 'Correct your previous console command'
+    set -l exit_code $status
+    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+    set -l fucked_up_command $history[1]
+    thefuck $fucked_up_command > $eval_script
+    . $eval_script
+    rm $eval_script
+    if test $exit_code -ne 0
+        history --delete $fucked_up_command
+    end
+end
